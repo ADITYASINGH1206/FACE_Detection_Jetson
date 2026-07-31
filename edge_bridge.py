3a9ca525c6f4e46c50dec1f6e8697a459037c97b
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-07-31 15:36:42 +0530

    chore: hardcode localtunnel API URL for easy Jetson deployment

--- edge_bridge.py
+++ edge_bridge.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 DB_PATH = '/dev/shm/attendance.db'
 import os
 BACKEND_IP = os.getenv('BACKEND_IP', '192.168.1.7')
-API_URL = os.getenv('API_URL', f'http://{BACKEND_IP}:3000/api/v1/events/attendance')
+API_URL = os.getenv('API_URL', 'https://pink-mangos-yell.loca.lt/api/v1/events/attendance')
 API_KEY = 'JETSON_DEV_KEY'
 
 HEADERS = {
